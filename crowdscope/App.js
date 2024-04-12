0806e2c6293c3e3d95acae71cbefff9fe88cefbe
chelsey tao <chelsey.tao@gmail.com>
2024-04-12 17:37:05 -0500

    mapview

--- crowdscope/App.js
+++ crowdscope/App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const[mapRegion,setMapRegion] =useState({
+    latitude: 42.055984,
+    longitude: -87.675171,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005* 2.16667
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView style={styles.map}
+        region = {mapRegion}>
+        <Marker coordinate = {mapRegion} title='Marker'/>
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+     
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
